8de3e69790706adc43e480d6dfd8525c8a301728
marchalbersma <contact@marchalbersma.com>
2026-08-29 00:56:02 +0200

    Add compiler

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,3 +1,14 @@
+use crate::compiler::analyzer::Analyzer;
+use crate::compiler::asm::generator::Generator;
+use crate::compiler::asm::writer::Writer;
+use crate::compiler::parser::Parser;
+use crate::compiler::symbol::SymbolTable;
+use ast::File;
+use std::error::Error;
+use std::fs;
+use std::path::Path;
+use std::process::Command;
+
 pub mod analyzer;
 pub mod asm;
 pub mod ast;
@@ -7,3 +18,113 @@ pub mod parser;
 pub mod span;
 pub mod symbol;
 pub mod token;
+
+pub struct Compiler<'a> {
+    build_dir: &'a str,
+    debug: bool,
+}
+
+impl<'a> Compiler<'a> {
+    pub fn new(build_dir: &'a str, debug: bool) -> Self {
+        Self { build_dir, debug }
+    }
+
+    pub fn compile_and_run(&self, src_dir: &str) -> Result<(), Box<dyn Error>> {
+        let name = self.extract_name(src_dir);
+
+        let src_path = format!("{}/main.ox", src_dir);
+        let asm_path = format!("{}/{}.asm", self.build_dir, name);
+        let obj_path = format!("{}/{}.obj", self.build_dir, name);
+        let exe_path = format!("{}/{}.exe", self.build_dir, name);
+
+        let src = fs::read_to_string(src_path)?;
+
+        let file = self.parse(&src);
+        self.analyze(file);
+
+        let asm = self.generate_asm();
+
+        fs::create_dir_all(self.build_dir)?;
+        fs::write(&asm_path, asm)?;
+
+        self.assemble(&asm_path, &obj_path)?;
+        self.link(&obj_path, &exe_path)?;
+
+        self.run(&exe_path)?;
+
+        Ok(())
+    }
+
+    fn extract_name<'b>(&self, src_dir: &'b str) -> &'b str {
+        Path::new(src_dir)
+            .file_name()
+            .and_then(|name| name.to_str())
+            .unwrap()
+    }
+
+    fn parse(&self, src: &str) -> File {
+        let mut parser = Parser::new(src);
+        let file = parser.parse();
+
+        if self.debug {
+            println!("AST: {:?}", file);
+        }
+
+        file
+    }
+
+    fn analyze(&self, file: File) -> SymbolTable {
+        let mut analyzer = Analyzer::new(file);
+        let symbols = analyzer.analyze();
+
+        if self.debug {
+            println!("Functions: {:?}", symbols.funcs());
+        }
+
+        symbols
+    }
+
+    fn generate_asm(&self) -> String {
+        let generator = Generator;
+        let file = generator.generate();
+
+        if self.debug {
+            println!("Assembly AST: {:?}", file);
+        }
+
+        Writer::write(&file)
+    }
+
+    fn assemble(&self, asm_path: &str, obj_path: &str) -> Result<(), Box<dyn Error>> {
+        Command::new("bin/nasm/nasm")
+            .arg(asm_path)
+            .arg("-o")
+            .arg(obj_path)
+            .arg("-f")
+            .arg("win64")
+            .output()
+            .map_err(|error| format!("Failed to assemble: {}", error))?;
+
+        Ok(())
+    }
+
+    fn link(&self, obj_path: &str, exe_path: &str) -> Result<(), Box<dyn Error>> {
+        Command::new("bin/lld/lld-link")
+            .arg(obj_path)
+            .arg(format!("-out:{}", exe_path))
+            .arg("-entry:__entry__")
+            .arg("-subsystem:console")
+            .arg("-defaultlib:kernel32.lib")
+            .output()
+            .map_err(|error| format!("Failed to link: {}", error))?;
+
+        Ok(())
+    }
+
+    fn run(&self, exe_path: &str) -> Result<(), Box<dyn Error>> {
+        let output = Command::new(exe_path).output()?;
+        println!("Exit code: {}", output.status.code().unwrap());
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,11 @@
-use crate::compiler::analyzer::Analyzer;
-use crate::compiler::asm::generator::Generator;
-use crate::compiler::asm::writer::Writer;
-use crate::compiler::parser::Parser;
-use std::fs;
+use crate::compiler::Compiler;
 
 pub mod compiler;
 
 fn main() {
-    let src = fs::read_to_string("examples/main/main.ox").expect("Failed to read source file");
+    let compiler = Compiler::new("build", false);
 
-    let mut parser = Parser::new(&src);
-    let file = parser.parse();
-
-    println!("AST: {:?}", file);
-
-    let mut analyzer = Analyzer::new(file);
-    let symbols = analyzer.analyze();
-
-    println!("Functions: {:?}", symbols.funcs());
-
-    let asm_generator = Generator;
-    let asm_file = asm_generator.generate();
-
-    println!("Assembly AST: {:?}", asm_file);
-
-    let asm = Writer::write(&asm_file);
-
-    println!("Assembly: {:?}", asm);
-
-    let build_dir = "build";
-
-    if !fs::exists(build_dir).expect("Failed to check if build directory exists") {
-        fs::create_dir(build_dir).expect("Failed to create build directory");
+    if let Err(error) = compiler.compile_and_run("examples/main") {
+        eprintln!("Failed to compile: {}", error);
     }
-
-    fs::write(format!("{}/main.asm", build_dir), asm).expect("Failed to write assembly file");
 }
